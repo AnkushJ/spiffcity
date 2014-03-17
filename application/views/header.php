dd8a8a2d799851eb491250739589ad6d204815f0
Derek Stock <derek.stock@saydesign.com>
2014-03-18 02:10:21 +0530

    first commit

---
+++ application/views/header.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<head>
+<!-- start: Meta -->
+  <meta charset="utf-8">
+  <title>Wecome to Get Spiffed - Your social currency engine</title>
+  <meta name="description" content="Get Spiffed - Your social currency engine">
+  <meta name="keywords" content="Get Spiffed - Your social currency engine">
+  <meta name="author" content="Get Spiffed - Your social currency engine">
+  <!-- end: Meta -->
+  <!-- start: Mobile Specific -->
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <!-- end: Mobile Specific -->
+  <!-- start: Facebook Open Graph -->
+  <meta property="og:title" content="">
+  <meta property="og:description" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+  <!-- end: Facebook Open Graph -->
+  <!-- start: CSS -->
+  <link href="css/bootstrap.css" rel="stylesheet">
+  <link href="css/bootstrap-responsive.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <link href="css/parallax-slider.css" rel="stylesheet">
+  
+
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Sans:400,700">
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Droid+Serif">
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Boogaloo">
+  <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Economica:700,400italic">
+  <!-- end: CSS -->
+  <!-- Le HTML5 shim, for IE6-8 support of HTML5 elements -->
+  <!--[if lt IE 9]>
+  <script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+      <![endif]-->
+  <script src="js/jquery-1.8.2.js"></script>
+  <script src="js/jquery.raty.min.js"></script>
+</head>
+
+<body>
+  <header>
+    <div class="container">
+    <!--start: Row -->
+    <div class="row">
+      <!--start: Logo -->
+      <div class="logo span3"> <i class="ico-charts circle"></i><a class="brand" href="index.html">Get<span>Spiffed</span>.</a> </div>
+      <!--end: Logo -->
+      <!--start: Navigation -->
+      <div class="span9">
+        <div class="navbar navbar-inverse">
+          <div class="navbar-inner"> <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span> </a>
+            <div class="nav-collapse collapse">
+              <ul class="nav">
+                <li class="active"> <a href="#">Home</a> </li>
+                <li><a href="redeem.html">Redeem</a></li>
+                <li><a href="people.html">People</a></li>
+                <li><a href="apps.html">Apps</a></li>
+                <li><a href="#" id="home-login">Login</a></li>
+               <!--  <li><a href="#" id="newpage-login">Login</a></li>-->
+                
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--end: Navigation -->
+    </div>
+    <!--end: Row -->
+  </div>
+  </header>
+</body>
